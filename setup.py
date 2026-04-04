296a4c929b203bdb22791646944aad5f23b6345e
Aayush Gauba <55630226+aayushgauba@users.noreply.github.com>
2026-04-04 11:05:37 -0500

    Bug Fixes

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (HERE / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="aiwaf",
-    version="0.1.9.6.3",
+    version="0.1.9.6.4",
     description="AI‑driven, self‑learning Web Application Firewall for Django",
     long_description=long_description,
     long_description_content_type="text/markdown",
